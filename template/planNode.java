d83b76569ca7ce8ef551a9f5c126fb4cd898a78d
rkempter <renato.kempter@gmail.com>
2012-10-25 16:15:16 +0200

    plan node class

---
+++ template/planNode.java
@@ -0,0 +1,25 @@
+package template;
+
+/* import table */
+import java.util.ArrayList;
+
+import logist.topology.Topology.City;
+
+public class planNode {
+	private planNode parent;
+	private ArrayList<City> nodeState;
+	private int capacity;
+	private int costs;
+	
+	public planNode(ArrayList<City> _nodeState, int _capacity, int _costs)
+	{
+		nodeState = _nodeState;
+		capacity = _capacity;
+		costs = _costs;
+	}
+	
+	public 
+
+	
+	
+}
