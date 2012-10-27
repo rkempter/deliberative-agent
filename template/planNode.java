101958ae4da48c18caf21158fee5e60fa5251cb3
rkempter <renato.kempter@gmail.com>
2012-10-27 11:57:22 +0200

    Model simplification. To do: Once a task arrives at delivered city -> change its current state

--- template/planNode.java
+++ template/planNode.java
@@ -59,25 +59,17 @@ public class planNode {
 		System.out.println("parent node: "+parentCity);
 		ArrayList<planNode> childNodes = new ArrayList<planNode>();
 		
-		// Move to neighbours
-		List<City> neighbours = vehiculePos.neighbors();
-		Iterator<City> iterator = neighbours.iterator();
+		ArrayList<planNode> newChildren = expandOverNeighbours(nodeState, capacity);
+		childNodes.addAll(newChildren);
 		
-		while(iterator.hasNext()) {
-			City neighbour = iterator.next();
-			System.out.println("to: "+neighbour);
-			// Calculate cost
-			int newCost = (int) (neighbour.distanceTo(parentCity) * vehicle.costPerKm());
-			// create new child node
-			planNode child = new planNode(vehicle, neighbour, nodeState, capacity, newCost);
-			childNodes.add(child);
-		}
-		
-		int nbrTasks = 3;
+		// How many tasks in this city?
+		ArrayList<Integer> subState = createSubState(nodeState);
+		int nbrTasks = subState.size();
 		int nbrSubStates = (int) Math.pow(2,nbrTasks);
 		
 		// Loop through all substates
 		for(int i = 0; i < nbrSubStates; i++) {
+			int newCapacity = capacity;
 			// Loop through the task list and select the right ones
 			/**
 			 * Create a table in format
@@ -94,8 +86,8 @@ public class planNode {
 				if(i % j < (int) Math.pow(2, j)) {
 					
 					Task currentTask = (Task) childNodeState.get(j).get(0);
-					if(capacity + currentTask.weight < totalCapacity) {
-						capacity += currentTask.weight;
+					if(newCapacity + currentTask.weight < totalCapacity) {
+						newCapacity += currentTask.weight;
 						childNodeState.get(j).set(1, PICKEDUP);
 						System.out.print("x |");
 					} else {
@@ -108,20 +100,57 @@ public class planNode {
 			
 			if(validState) {
 				// Go through all the cities
-				iterator = neighbours.iterator();
-				
-				while(iterator.hasNext()) {
-					City neighbour = iterator.next();
-					// Calculate cost
-					int newCost = (int) (neighbour.distanceTo(parentCity) * vehicle.costPerKm());
-					// create new child node
-					planNode child = new planNode(vehicle, neighbour, childNodeState, capacity, newCost);
-					childNodes.add(child);
-				}
+				newChildren = expandOverNeighbours(childNodeState, newCapacity);
+				childNodes.addAll(newChildren);
 			}
 		}
 		
 		return childNodes;
+	}
+	
+	/**
+	 * All tasks, that are on the current city (vehicle position)
+	 * and haven't been moved yet, are extracted from the currentState
+	 * into a subState. Based on the substate, we calculate the possible
+	 * next global states
+	 * 
+	 * @param currentState
+	 * @return subState
+	 */
+	
+	public ArrayList<Integer> createSubState(ArrayList<ArrayList<Object>> currentState) {
+		ArrayList<Integer> subState = new ArrayList<Integer>();
+		
+		int size = currentState.size();
+		for(int i = 0; i < size; i++) {
+			ArrayList<Object> current = currentState.get(i);
+			Task task = (Task) current.get(0);
+			Integer pos = new Integer(i);
+			if(task.pickupCity == parentCity && current.get(1).equals(INITSTATE)) {
+				subState.add(pos);
+			}
+		}
+		
+		return subState;
+	}
+	
+	public ArrayList<planNode> expandOverNeighbours(ArrayList<ArrayList<Object>> newState, int newCapacity) {
+		ArrayList<planNode> children = new ArrayList<planNode>();
+		
+		List<City> neighbours = vehiculePos.neighbors();
+		Iterator<City> iterator = neighbours.iterator();
+		
+		while(iterator.hasNext()) {
+			City neighbour = iterator.next();
+			System.out.println("to: "+neighbour);
+			// Calculate cost
+			int newCost = (int) (neighbour.distanceTo(parentCity) * vehicle.costPerKm());
+			// create new child node
+			planNode child = new planNode(vehicle, neighbour, newState, newCapacity, newCost);
+			children.add(child);
+		}
+		
+		return children;
 	}
 	
 }
